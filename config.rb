60e1838012d06e9fd5b61f44dd9584ce0bacc30a
Andy Mangold <andy@andymangold.com>
2017-07-01 17:37:42 -0400

    Remove no longer necessary soundcloudID from episode template

--- config.rb
+++ config.rb
@@ -64,7 +64,7 @@ set :relative_links, true
 # Create pages for each episode dynamically
 data.episodes.each do |episode|
   if episode.published == true
-    proxy "/episodes/#{episode.id}.html", "/episodes/template.html", :locals => { :id => episode.id, :date => episode.date, :title => episode.title, :soundcloudID => episode.soundcloudID, :transcript => episode.transcript, :contributorSlugs => episode.contributorSlugs, :showNotes => episode.showNotes, :links => episode.links }
+    proxy "/episodes/#{episode.id}.html", "/episodes/template.html", :locals => { :id => episode.id, :date => episode.date, :title => episode.title, :transcript => episode.transcript, :contributorSlugs => episode.contributorSlugs, :showNotes => episode.showNotes, :links => episode.links }
   end
 end
 
